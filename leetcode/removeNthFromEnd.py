922ff624b0ac533809fc2af77c960db897b402f7
Peter Ling <peterl732@hotmail.com>
2020-05-26 00:17:13 -0400

    partial sol

--- leetcode/removeNthFromEnd.py
+++ leetcode/removeNthFromEnd.py
@@ -25,34 +25,42 @@ Could you do this in one pass?
 #         self.val = val
 #         self.next = next
 
-def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+def removeNthFromEnd(head, n):
         
-        if head.next == None:
-            return []
-        
-        node = head.next
-        
-        count = 2
+        #node = head.next
+        i = 0
+        count = 1
 
-        while node.next != None:
-            node = node.next
+        #while node.next != None:
+        while i < len(head):
+            #node = node.next
+            i+=1
             count += 1
         
-        node = head.next
-        c2 = 2
+        print(f"count:{count}")
+        #node = head.next
+        i = 0
+        c2 = 1
         
-        while node.next != None:
-            if c2 > count - n:
-                node.val = node.next.val
-                node.next = node.next.next
+        #while node.next != None:
+        while i < len(head):
+            if c2 >= count - n:
+                #node.val = node.next.val
+                #node.next = node.next.next
+                print(f"count-n:{count-n}")
+                print(f"c2:{c2}")
+                del head[c2 - 1]
                 return head
 
             c2 += 1
-            node = node.next
+            #node = node.next
+            i+=1
+
+        print(c2)
             
         return head
 
+print(removeNthFromEnd([1,2,3,4,5],2))
+#print(removeNthFromEnd([1],1))
 
-
-
-
+#1->2->3->5
